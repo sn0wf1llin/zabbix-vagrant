39a5a1dfa2b7ec8fa6bc3f72b02b88b8e29ecd65
Student <student@EPBYMINW5878.minsk.epam.com>
2019-02-15 15:52:16 +0300

    zabbix server vagrant;

--- Vagrantfile
+++ Vagrantfile
@@ -1,18 +1,18 @@
 Vagrant.configure(2) do |config|
 
-  config.vm.define "server" do |server|
-    server.vm.box = "sbeliakou/centos7"
+  config.vm.define "zabbix.lab.server" do |server|
+    server.vm.box = "sbeliakou/centos"
     config.vm.network "forwarded_port", guest: 80, host: 8080
-  	config.vm.network "public_network"
+  	config.vm.network "private_network", ip: "192.168.33.10"
 
     server.vm.provision "shell", path: "build.sh", args: "server", privileged: true
     server.vm.hostname = "zabbix.lab.server"
   end
 
-  # config.vm.define "agent" do |agent|
-  #   agent.vm.box = "sbeliakou/centos7"
+  # config.vm.define "zabbix.lab.agent" do |agent|
+  #   agent.vm.box = "sbeliakou/centos"
   #   config.vm.network "forwarded_port", guest: 80, host: 8080
-  # 	config.vm.network "public_network"
+  # 	config.vm.network "private_network", ip: "192.168.33.11"
 
   #   agent.vm.provision "shell", path: "build.sh", args: "agent", privileged: true
   #   agent.vm.hostname = "zabbix.lab.agent"
--- build.sh
+++ build.sh
@@ -5,24 +5,60 @@
 
 DATABASE_NAME="zabbix"
 
-function imain(){
-	yum install -y mariadb mariadb-server
-	/usr/bin/mysql_install_db --user=mysql
-	systemctl start mariadb
-
-	# check if db exists
-	if [ ! -d /var/lib/mysql/"$DATABASE_NAME" ]; then
-    	mysql -e "create database $DATABASE_NAME character set utf8 collate utf8_bin; grant all privileges on $DATABASE_NAME.* to $ZABBIX_USER@localhost identified by '$ZABBIX_PASSWORD'; quit;"
-	fi;
-	
-	yum install -y http://repo.zabbix.com/zabbix/3.4/rhel/7/x86_64/zabbix-agent-3.4.15-1.el7.x86_64.rpm 
-	yum install -y zabbix-web-mysql 
+function debug() {
+	echo "set number" > ~/.vimrc
+	echo "syntax on" >> ~/.vimrc
 
 }
 
 
+function imain(){
+	debug 
+
+	yum install -y lsof net-tools mariadb mariadb-server 1> /dev/null
+	/usr/bin/mysql_install_db --user=mysql
+
+	systemctl start mariadb ; systemctl enable mariadb
+
+	# check if db exists
+	if [ ! -d /var/lib/mysql/"$DATABASE_NAME" ]; then
+		mysql -uroot -e "create database $DATABASE_NAME character set utf8 collate utf8_bin ; GRANT ALL PRIVILEGES ON $DATABASE_NAME.* to $ZABBIX_USER@localhost identified by '$ZABBIX_PASSWORD';"
+
+	fi;
+	
+	yum install -y http://repo.zabbix.com/zabbix/3.4/rhel/7/x86_64/zabbix-release-3.4-1.el7.centos.noarch.rpm 1> /dev/null
+	yum install -y zabbix-server-mysql zabbix-web-mysql 1> /dev/null
+
+	zcat /usr/share/doc/zabbix-server-mysql-*/create.sql.gz | mysql -u"$ZABBIX_USER" -p"$ZABBIX_PASSWORD" $DATABASE_NAME
+
+	ZABBIX_CONF="/etc/zabbix/zabbix_server.conf"
+
+	# change server configuration
+	sed -i -E '/DBHost/s/^# //g; /DBPassword/s/^#{1,2} //; s/DBPassword=.*/DBPassword='$ZABBIX_PASSWORD'/' $ZABBIX_CONF
+	# check changes
+	cat $ZABBIX_CONF | grep -iE 'dbhost|dbname|dbuser|dbpassword'
+
+	systemctl start zabbix-server || echo "zabbix_server start [ failed ]" ; systemctl enable zabbix-server
+
+	PHP_CONF="/etc/httpd/conf.d/zabbix.conf"
+	# change web
+	sed -i '/date.timezone/s/# //g; /date.timezone/s/date.timezone.*/date.timezone Europe\/Minsk/' $PHP_CONF;
+	# check changes
+	cat $PHP_CONF | grep timezone
+
+	systemctl start firewalld
+	firewall-cmd --permanent --add-port=10051/tcp
+	firewall-cmd --reload
+
+	systemctl start httpd || echo "httpd start [ failed ]"
+	
+	lsof -i -P -n | grep -E '10051|10050' || echo "no ports LISTEN"
+}
+
+
 function mkagent() {
-	unset ZABBIX_USER, ZABBIX_PASSWORD
+	unset ZABBIX_USER ZABBIX_PASSWORD
+
 	export ZABBIX_USER="zabbix"
 	export ZABBIX_PASSWORD="agentpassword"
 
@@ -31,7 +67,8 @@ function mkagent() {
 }
 
 function mkserver() {
-	unset ZABBIX_USER, ZABBIX_PASSWORD
+	unset ZABBIX_USER ZABBIX_PASSWORD
+
 	export ZABBIX_USER="zabbix"
 	export ZABBIX_PASSWORD="serverpassword"
 
